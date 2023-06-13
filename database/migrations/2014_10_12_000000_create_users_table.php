a23d5627591cb4fd13c80ba2d03716f80af25e3f
Earnest <igbedeernest@gmail.com>
2023-06-13 20:49:19 +0100

    sharp

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('code')->default('007');
+            $table->string('phone_number')->nullable();
+            $table->string('skill')->nullable();
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
